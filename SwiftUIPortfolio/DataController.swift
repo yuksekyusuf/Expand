c36c4276ffab15a43c34dff4851b0301a939cf5f
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2022-01-02 15:00:23 -0500

    Create datacontroller

---
+++ SwiftUIPortfolio/DataController.swift
@@ -0,0 +1,63 @@
+//
+//  DataController.swift
+//  SwiftUIPortfolio
+//
+//  Created by Ahmet Yusuf Yuksek on 1/2/22.
+//
+
+import CoreData
+import SwiftUI
+
+
+class DataController: ObservableObject {
+    let container: NSPersistentCloudKitContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentCloudKitContainer(name: "Main")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                fatalError("Fatal error loading store: \(error.localizedDescription)")
+            }
+            
+            
+        }
+        
+    }
+    
+    static var preview: DataController = {
+        let dataController = DataController(inMemory: true)        
+        do {
+            try dataController.createSampleData()
+        } catch {
+            fatalError("Fatal error creating preview: \(error.localizedDescription)")
+        }
+        return dataController
+    }()
+    
+    func createSampleData() throws {
+        let viewContext = container.viewContext
+        
+        for i in 1...5 {
+            let project = Project(context: viewContext)
+            project.title = "Project \(i)"
+            project.items = []
+            project.creationDate = Date()
+            project.closed = Bool.random()
+            
+            for j in 1...10 {
+                let item = Item(context: viewContext)
+                item.title = "Item \(j)"
+                item.creationDate = Date()
+                item.completed = Bool.random()
+                item.project = project
+                item.priority = Int16.random(in: 1...3)
+            }
+        }
+        try viewContext.save()
+    }
+}
