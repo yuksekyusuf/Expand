0d14886ef572f90083a2c76a2747113c3f84463c
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2022-01-02 13:59:26 -0500

    Initial Commit

---
+++ SwiftUIPortfolio/SwiftUIPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPortfolioApp.swift
+//  SwiftUIPortfolio
+//
+//  Created by Ahmet Yusuf Yuksek on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
