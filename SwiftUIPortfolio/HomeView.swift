573c26c22473aa1858722ba3001186e51acfb9cb
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2022-01-03 22:16:08 -0500

    build initial UI and fetchrequest functionality

---
+++ SwiftUIPortfolio/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  SwiftUIPortfolio
+//
+//  Created by Ahmet Yusuf Yuksek on 1/3/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
