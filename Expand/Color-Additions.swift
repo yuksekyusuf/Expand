f4f01d7402a24e44c97342a3788eb3526fed4b51
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2022-01-10 22:46:44 -0500

    Update home screen

---
+++ Expand/Color-Additions.swift
@@ -0,0 +1,15 @@
+//
+//  Color-Additions.swift
+//  Expand
+//
+//  Created by Ahmet Yusuf Yuksek on 1/10/22.
+//
+
+import SwiftUI
+
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+    
+}
