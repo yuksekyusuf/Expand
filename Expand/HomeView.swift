d7d22f5d5c5f3305d984f29eeeab7c3d72da546c
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2022-03-14 16:59:24 +0100

    Refactor

--- Expand/HomeView.swift
+++ Expand/HomeView.swift
@@ -14,9 +14,7 @@ struct HomeView: View {
     
     @FetchRequest(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.title, ascending: true)], predicate: NSPredicate(format: "closed = false"))
     var projects: FetchedResults<Project>
-    
-    
-    
+
     let items: FetchRequest<Item>
     init() {
         let request: NSFetchRequest<Item> = Item.fetchRequest()
@@ -38,32 +36,14 @@ struct HomeView: View {
                 VStack(alignment: .leading) {
                     ScrollView(.horizontal, showsIndicators: false) {
                         LazyHGrid(rows: projectRows) {
-                            ForEach(projects) { project in
-                                VStack(alignment: .leading) {
-                                    Text("\(project.projectItems.count) items")
-                                        .font(.caption)
-                                        .foregroundColor(.secondary)
-                                    
-                                    Text(project.projectTitle)
-                                        .font(.title2)
-                                    
-                                    ProgressView(value: project.completionAmount)
-                                        .accentColor(Color(project.projectColor))
-                                }
-                                .padding()
-                                .background(Color.secondarySystemGroupedBackground)
-                                .cornerRadius(10)
-                                .shadow(color: Color.black.opacity(0.2), radius: 5)
-                                .accessibilityElement(children: .ignore)
-                                .accessibilityLabel("\(project.projectTitle), \(project.projectItems.count) items, \(project.completionAmount * 100, specifier: "%g")% complete.")
-                            }
+                            ForEach(projects, content: ProjectSummaryView.init)
                         }
                         .padding([.horizontal, .top])
                         .fixedSize(horizontal: false, vertical: true)
                     }
                     VStack(alignment: .leading) {
-                        list("Up next", for: items.wrappedValue.prefix(3))
-                        list("More to explore", for: items.wrappedValue.dropFirst(3))
+                        ItemListView(title: "Up next", items: items.wrappedValue.prefix(3))
+                        ItemListView(title: "More to explore", items: items.wrappedValue.dropFirst(3))
                     }
                     .padding(.horizontal)
                 }
@@ -71,44 +51,6 @@ struct HomeView: View {
             }
             .background(Color.systemGroupedBackground.ignoresSafeArea())
             .navigationTitle("Home")
-            
-        }
-    }
-    
-    @ViewBuilder func list(_ title: LocalizedStringKey, for items: FetchedResults<Item>.SubSequence) -> some View {
-        if items.isEmpty {
-            EmptyView()
-        } else {
-            Text(title)
-                .font(.headline)
-                .foregroundColor(.secondary)
-                .padding(.top)
-            
-            ForEach(items) { item in
-                NavigationLink(destination: EditItemView(item: item)) {
-                    HStack(spacing: 20) {
-                        Circle()
-                            .stroke(Color(item.project?.projectColor ?? "Light Blue"), lineWidth: 3)
-                            .frame(width: 44, height: 44)
-                        
-                        VStack(alignment: .leading) {
-                            Text(item.itemTitle)
-                                .font(.title2)
-                                .foregroundColor(.primary)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            if item.itemDetail.isEmpty == false {
-                                Text(item.itemDetail)
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                    }
-                    .padding()
-                    .background(Color.secondarySystemGroupedBackground)
-                    .cornerRadius(10)
-                    .shadow(color: Color.black.opacity(0.2), radius: 5)
-                    
-                }
-            }
         }
     }
 }
